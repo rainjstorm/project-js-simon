9cab53a720145b308faac3b61744b949a39680ec
Cadie B. McNaboe <camcnaboe@davidson.edu>
2020-07-16 00:19:32 -0400

    new changes in js

--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,15 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
+
+document.querySelector("simon-button.green").innerHTML = Boolean(10 > 9){
+
+  var playing = true;
+  // var playing = false;
+  // var startButton;
+  //To begin game, var is true; on loading page, var is false
+
+  if (playing = true) {
+    document.querySelector("simon-button.green.lightUp")
+
+  }
+}
