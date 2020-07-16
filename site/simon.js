b8e5ef4f8eb9c8c385003b294873a683a4edc905
Tinley Bikos <rainjstorm@gmail.com>
2020-07-15 22:22:10 -0700

    added start button icon, does not work yet

--- site/simon.js
+++ site/simon.js
@@ -1,15 +1,19 @@
 // This file contains the game logic.
 // All the event-listening should happen in buttons.js
 
-document.querySelector("simon-button.green").innerHTML = Boolean(10 > 9){
+document.querySelector("simon-button.green").innerHTML = Boolean(10 > 9){}
 
-  var playing = true;
-  // var playing = false;
-  // var startButton;
+  var playing = false;
+  var startButton;
   //To begin game, var is true; on loading page, var is false
 
+
+  //use if statements to initialize game functions after start button is pressed
   if (playing = true) {
     document.querySelector("simon-button.green.lightUp")
 
+  } else {
+    function() {
+      alert("Click the button to start the game.");
+    }
   }
-}
